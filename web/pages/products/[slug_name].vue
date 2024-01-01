45b85c578999e8f3f150c629a4b739afb843c5a3
duyhuynh <duyhuynh55110@gmail.com>
2024-01-01 15:29:21 +0700

    SKOTE-18: fix UI product detail page

--- web/pages/products/[slug_name].vue
+++ web/pages/products/[slug_name].vue
@@ -159,15 +159,15 @@
                     <div class="text-center">
                       <button
                         type="button"
-                        class="btn btn-primary waves-effect waves-light mt-2 mr-1"
+                        class="btn btn-primary waves-effect waves-light mt-2 me-1"
                       >
-                        <i class="bx bx-cart mr-2"></i> Add to cart
+                        <i class="bx bx-cart me-2"></i> Add to cart
                       </button>
                       <button
                         type="button"
-                        class="btn btn-success waves-effect mt-2 waves-light"
+                        class="btn btn-success waves-effect waves-light mt-2"
                       >
-                        <i class="bx bx-shopping-bag mr-2"></i>Buy now
+                        <i class="bx bx-shopping-bag me-2"></i>Buy now
                       </button>
                     </div>
                   </div>
@@ -177,23 +177,26 @@
 
             <div class="col-xl-6">
               <div class="mt-4 mt-xl-3">
-                <a href="#" class="text-primary">Headphone</a>
+                <a href="#" class="text-primary me-1">#Headphone</a>
+                <a href="#" class="text-primary me-1">#Electronic</a>
                 <h4 class="mt-1 mb-3">Wireless Headphone (Black)</h4>
 
-                <p class="text-muted float-left mr-3">
-                  <span class="bx bx-star text-warning"></span>
-                  <span class="bx bx-star text-warning"></span>
-                  <span class="bx bx-star text-warning"></span>
-                  <span class="bx bx-star text-warning"></span>
-                  <span class="bx bx-star"></span>
-                </p>
-                <p class="text-muted mb-4">( 152 Customers Review )</p>
+                <div class="d-flex">
+                  <p class="text-muted float-left me-3">
+                    <span class="bx bx-star text-warning"></span>
+                    <span class="bx bx-star text-warning"></span>
+                    <span class="bx bx-star text-warning"></span>
+                    <span class="bx bx-star text-warning"></span>
+                    <span class="bx bx-star"></span>
+                  </p>
 
+                  <p class="text-muted mb-4">( 152 Customers Review )</p>
+                </div>
                 <h6 class="text-success text-uppercase">20 % Off</h6>
                 <h5 class="mb-4">
                   Price :
-                  <span class="text-muted mr-2"><del>$240 USD</del></span>
-                  <b>$225 USD</b>
+                  <!-- <span class="text-muted me-2"><del>$240 USD</del></span> -->
+                  <b>$225</b>
                 </h5>
                 <p class="text-muted mb-4">
                   To achieve this, it would be necessary to have uniform grammar
@@ -205,19 +208,19 @@
                     <div>
                       <p class="text-muted">
                         <i
-                          class="bx bx-unlink font-size-16 align-middle text-primary mr-1"
+                          class="bx bx-unlink font-size-16 align-middle text-primary me-1"
                         ></i>
                         Wireless
                       </p>
                       <p class="text-muted">
                         <i
-                          class="bx bx-shape-triangle font-size-16 align-middle text-primary mr-1"
+                          class="bx bx-shape-triangle font-size-16 align-middle text-primary me-1"
                         ></i>
                         Wireless Range : 10m
                       </p>
                       <p class="text-muted">
                         <i
-                          class="bx bx-battery font-size-16 align-middle text-primary mr-1"
+                          class="bx bx-battery font-size-16 align-middle text-primary me-1"
                         ></i>
                         Battery life : 6hrs
                       </p>
@@ -227,52 +230,18 @@
                     <div>
                       <p class="text-muted">
                         <i
-                          class="bx bx-user-voice font-size-16 align-middle text-primary mr-1"
+                          class="bx bx-user-voice font-size-16 align-middle text-primary me-1"
                         ></i>
                         Bass
                       </p>
                       <p class="text-muted">
                         <i
-                          class="bx bx-cog font-size-16 align-middle text-primary mr-1"
+                          class="bx bx-cog font-size-16 align-middle text-primary me-1"
                         ></i>
                         Warranty : 1 Year
                       </p>
                     </div>
                   </div>
-                </div>
-
-                <div class="product-color">
-                  <h5 class="font-size-15">Color :</h5>
-                  <a href="#" class="active">
-                    <div class="product-color-item border rounded">
-                      <img
-                        src="assets/images/product/img-7.png"
-                        alt=""
-                        class="avatar-md"
-                      />
-                    </div>
-                    <p>Black</p>
-                  </a>
-                  <a href="#">
-                    <div class="product-color-item border rounded">
-                      <img
-                        src="assets/images/product/img-7.png"
-                        alt=""
-                        class="avatar-md"
-                      />
-                    </div>
-                    <p>Blue</p>
-                  </a>
-                  <a href="#">
-                    <div class="product-color-item border rounded">
-                      <img
-                        src="assets/images/product/img-7.png"
-                        alt=""
-                        class="avatar-md"
-                      />
-                    </div>
-                    <p>Gray</p>
-                  </a>
                 </div>
               </div>
             </div>
@@ -317,7 +286,7 @@
             <div class="media border-bottom">
               <img
                 src="assets/images/users/avatar-2.jpg"
-                class="avatar-xs mr-3 rounded-circle"
+                class="avatar-xs me-3 rounded-circle"
                 alt="img"
               />
               <div class="media-body">
@@ -326,18 +295,18 @@
                   If several languages coalesce, the grammar of the resulting
                   language.
                 </p>
-                <ul class="list-inline float-sm-right">
+                <ul class="list-inline float-sm-right mb-2">
                   <li class="list-inline-item">
-                    <a href="#"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+                    <a href="#"><i class="far fa-thumbs-up me-1"></i> Like</a>
                   </li>
                   <li class="list-inline-item">
                     <a href="#"
-                      ><i class="far fa-comment-dots mr-1"></i> Comment</a
+                      ><i class="far fa-comment-dots me-1"></i> Comment</a
                     >
                   </li>
                 </ul>
                 <div class="text-muted font-size-12">
-                  <i class="far fa-calendar-alt text-primary mr-1"></i> 5 hrs
+                  <i class="far fa-calendar-alt text-primary me-1"></i> 5 hrs
                   ago
                 </div>
               </div>
@@ -345,7 +314,7 @@
             <div class="media mt-3 border-bottom">
               <img
                 src="assets/images/users/avatar-4.jpg"
-                class="avatar-xs mr-3 rounded-circle"
+                class="avatar-xs me-3 rounded-circle"
                 alt="img"
               />
               <div class="media-body">
@@ -354,24 +323,24 @@
                   To an English person, it will seem like simplified English, as
                   a skeptical Cambridge
                 </p>
-                <ul class="list-inline float-sm-right">
+                <ul class="list-inline float-sm-right mb-2">
                   <li class="list-inline-item">
-                    <a href="#"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+                    <a href="#"><i class="far fa-thumbs-up me-1"></i> Like</a>
                   </li>
                   <li class="list-inline-item">
                     <a href="#"
-                      ><i class="far fa-comment-dots mr-1"></i> Comment</a
+                      ><i class="far fa-comment-dots me-1"></i> Comment</a
                     >
                   </li>
                 </ul>
                 <div class="text-muted font-size-12">
-                  <i class="far fa-calendar-alt text-primary mr-1"></i> 07 Oct,
+                  <i class="far fa-calendar-alt text-primary me-1"></i> 07 Oct,
                   2019
                 </div>
                 <div class="media mt-4">
                   <img
                     src="assets/images/users/avatar-5.jpg"
-                    class="avatar-xs mr-3 rounded-circle"
+                    class="avatar-xs me-3 rounded-circle"
                     alt="img"
                   />
                   <div class="media-body">
@@ -380,20 +349,20 @@
                       Their separate existence is a myth. For science, music,
                       sport, etc.
                     </p>
-                    <ul class="list-inline float-sm-right">
+                    <ul class="list-inline float-sm-right mb-2">
                       <li class="list-inline-item">
                         <a href="#"
-                          ><i class="far fa-thumbs-up mr-1"></i> Like</a
+                          ><i class="far fa-thumbs-up me-1"></i> Like</a
                         >
                       </li>
                       <li class="list-inline-item">
                         <a href="#"
-                          ><i class="far fa-comment-dots mr-1"></i> Comment</a
+                          ><i class="far fa-comment-dots me-1"></i> Comment</a
                         >
                       </li>
                     </ul>
                     <div class="text-muted font-size-12">
-                      <i class="far fa-calendar-alt text-primary mr-1"></i> 08
+                      <i class="far fa-calendar-alt text-primary me-1"></i> 08
                       Oct, 2019
                     </div>
                   </div>
@@ -402,7 +371,7 @@
             </div>
 
             <div class="media mt-3 border-bottom">
-              <div class="avatar-xs mr-3">
+              <div class="avatar-xs me-3">
                 <span
                   class="avatar-title bg-soft-primary text-primary rounded-circle font-size-16"
                 >
@@ -415,18 +384,18 @@
                   Everyone realizes why a new common language would be
                   desirable.
                 </p>
-                <ul class="list-inline float-sm-right">
+                <ul class="list-inline float-sm-right mb-2">
                   <li class="list-inline-item">
-                    <a href="#"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+                    <a href="#"><i class="far fa-thumbs-up me-1"></i> Like</a>
                   </li>
                   <li class="list-inline-item">
                     <a href="#"
-                      ><i class="far fa-comment-dots mr-1"></i> Comment</a
+                      ><i class="far fa-comment-dots me-1"></i> Comment</a
                     >
                   </li>
                 </ul>
                 <div class="text-muted font-size-12">
-                  <i class="far fa-calendar-alt text-primary mr-1"></i> 05 Oct,
+                  <i class="far fa-calendar-alt text-primary me-1"></i> 05 Oct,
                   2019
                 </div>
               </div>
@@ -468,7 +437,7 @@
                           <i class="bx bx-star"></i>
                         </p>
                         <h5 class="my-0">
-                          <span class="text-muted mr-2"><del>$240</del></span>
+                          <span class="text-muted me-2"><del>$240</del></span>
                           <b>$225</b>
                         </h5>
                       </div>
@@ -503,7 +472,7 @@
                           <i class="bx bx-star"></i>
                         </p>
                         <h5 class="my-0">
-                          <span class="text-muted mr-2"><del>$150</del></span>
+                          <span class="text-muted me-2"><del>$150</del></span>
                           <b>$145</b>
                         </h5>
                       </div>
@@ -538,7 +507,7 @@
                           <i class="bx bx-star"></i>
                         </p>
                         <h5 class="my-0">
-                          <span class="text-muted mr-2"><del>$138</del></span>
+                          <span class="text-muted me-2"><del>$138</del></span>
                           <b>$135</b>
                         </h5>
                       </div>
